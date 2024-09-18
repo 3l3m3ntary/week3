a75df1e60d0171a3b63bd547efcdc908a1431efc
3l3m3nt5 <t2site00@students.oamk.fi>
2024-09-18 21:59:43 +0300

    returnable

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Button , Appbar} from 'react-native-paper';
+import { getHeaderTitle } from '@react-navigation/elements';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          header: (props) => <CustomNavigationBar {...props} />,
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Second Home" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -15,6 +27,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={style.container}>
+      <Text>Home Screen</Text>
+      <Button mode="contained" onPress={() => navigation.navigate('Second Home')}>
+      Go to another screen
+      </Button>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={style.container}>
+      <Text>Second Home</Text>
+    </View>
+  );
+}
+
+function CustomNavigationBar({ navigation, route, options, back }) {
+  const title = getHeaderTitle(options, route.name);
+
+  return (
+    <Appbar.Header>
+      {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+      <Appbar.Content title={title} />
+    </Appbar.Header>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
